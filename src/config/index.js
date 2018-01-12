4d4e44fc15a93cae0eef0d574be79512d97ee4f8
SIMRAN1 <simranpreetkaurkhalsa13@gmail.com>
2018-01-12 18:42:59 +0530

    modifying url

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,6 @@ export default {
   // "port": 3005,
   // "mongoUrl": "mongodb://localhost:27017/chat-api",
   "port": process.env.PORT,
-  "mongoUrl": process.env.MONGODB_URI,
+  "mongoUrl": "mongodb://simran:123456@ds251287.mlab.com:51287/chattymac",
   "bodyLimit": "100kb"
 }
